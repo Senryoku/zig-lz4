fa8dca81f459be76fdaf6b99f98d2005101856fc
Senryoku <maretverdant@gmail.com>
2026-04-12 12:39:24 +0200

    [WIP] Update for zig 0.16

--- build.zig
+++ build.zig
@@ -40,11 +40,11 @@ pub fn build(b: *std.Build) void {
     };
 
     for (HEADER_DIRS) |dir| {
-        lib.addIncludePath(lz4_dependency.path(dir));
+        lib.root_module.addIncludePath(lz4_dependency.path(dir));
     }
-    lib.linkLibCpp();
+    lib.root_module.link_libcpp = true;
     for (SOURCE_FILES) |file| {
-        lib.addCSourceFile(.{ .file = lz4_dependency.path(file), .flags = &FLAGS });
+        lib.root_module.addCSourceFile(.{ .file = lz4_dependency.path(file), .flags = &FLAGS });
     }
 
     lib.installHeader(lz4_dependency.path("lib/lz4.h"), "lz4.h");
@@ -61,8 +61,8 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
-    lib_unit_tests.linkLibrary(lib);
-    lib_unit_tests.addIncludePath(lz4_dependency.path("lib"));
+    lib_unit_tests.root_module.linkLibrary(lib);
+    lib_unit_tests.root_module.addIncludePath(lz4_dependency.path("lib"));
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
@@ -81,8 +81,8 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
-    docs_obj.linkLibrary(lib);
-    docs_obj.addIncludePath(lz4_dependency.path("lib"));
+    docs_obj.root_module.linkLibrary(lib);
+    docs_obj.root_module.addIncludePath(lz4_dependency.path("lib"));
 
     const install_docs = b.addInstallDirectory(.{
         .source_dir = docs_obj.getEmittedDocs(),
